e4adfe4c5c69893ba979f692901adc66202328b1
AdrianEmaST <desarrolloadrianjr@gmail.com>
2024-11-11 12:11:04 -0300

    Add: Verificacion edad mobile | Fix: ActiveInNavbar

--- frontend/src/app/ageverification/page.tsx
+++ frontend/src/app/ageverification/page.tsx
@@ -5,7 +5,13 @@ import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { HiOutlineX } from "react-icons/hi"; // Íconos para el menú
-import { Logo, HumoEdadIzquierda, HumoEdadDerecha, Ellipse, EllipseDerecha } from "@/public";
+import {
+  Logo,
+  HumoEdadIzquierda,
+  HumoEdadDerecha,
+  Ellipse,
+  EllipseDerecha,
+} from "@/public";
 
 const AgeVerification = () => {
   const router = useRouter();
@@ -29,36 +35,39 @@ const AgeVerification = () => {
 
   return (
     <div className="fixed inset-0 bg-background flex justify-center items-center text-white z-50">
-        {/* Humos*/}
-      <div className="absolute top-14 left-0 justify-between">
+      {/* Humos*/}
+      <div className="absolute lg:top-14 top-4 left-0 justify-between">
         <div>
           <Image
             src={HumoEdadIzquierda}
             alt="HumoEdadIzquierda"
-            width={181}
-            height={320}
+            width={90}
+            height={160}
+            className="lg:w-[181px] lg:h-[320px]"
           />
         </div>
       </div>
-      <div className="absolute bottom-16 right-0 justify-between">
+      <div className="absolute lg:bottom-16 bottom-6 right-0 justify-between">
         <div>
           {" "}
           <Image
             src={HumoEdadDerecha}
             alt="HumoEdadDerecha"
-            width={181}
-            height={320}
+            width={90}
+            height={160}
+            className="lg:w-[181px] lg:h-[320px]"
           />
         </div>
       </div>
-         {/* Circulos */}
-         <div className="absolute bottom-0 left-0 justify-between">
+      {/* Circulos */}
+      <div className="absolute bottom-0 left-0 justify-between">
         <div>
           <Image
             src={Ellipse}
             alt="HumoEdadIzquierda"
-            width={419}
-            height={419}
+            width={205}
+            height={205}
+            className="lg:w-[419px] lg:h-[419px]"
           />
         </div>
       </div>
@@ -68,37 +77,37 @@ const AgeVerification = () => {
           <Image
             src={EllipseDerecha}
             alt="HumoEdadDerecha"
-            width={419}
-            height={419}
+            width={205}
+            height={205}
+            className="lg:w-[419px] lg:h-[419px]"
           />
         </div>
       </div>
 
-      <div className="w-[400px] h-[190px] px-5 pt-5 pb-[30px] bg-[#e05bff]/20 rounded-[15px] border-2 border-[#e05bff] justify-start items-start gap-5 inline-flex">
-        <Image
-          src={Logo}
-          alt="Logo"
-          width={49}
-          height={35}
-          className="w-[49.05px] h-[35px] rounded-[0.05px]"
-        />
-
-        <div className="grow shrink basis-0 h-[140px] justify-start items-start gap-2.5 flex">
-          <div className="grow shrink basis-0 flex-col justify-start items-start gap-[15px] inline-flex">
-            <div className="self-stretch h-[85px] flex-col justify-start items-start gap-[5px] flex">
-              <div className="self-stretch text-[#f6f7f8] text-lg font-semibold leading-[30px]">
+      <div className="lg:w-[400px] lg:h-[190px] w-[310px] h-[230px] px-5 lg:pt-5 lg:pb-[30px] bg-[#e05bff]/20 rounded-[15px] border-2 border-[#e05bff] lg:flex-row lg:justify-start lg:items-start lg:gap-5 lg:inline-flex flex flex-col justify-center items-center relative">
+        <Image src={Logo} alt="Logo" width={49} height={35} />
+        <button onClick={() => handleAgeVerification(false)}>
+          <HiOutlineX
+            size={24}
+            className=" lg:hidden block absolute top-2 right-2"
+          />
+        </button>
+        <div className=" lg:h-[140px] lg:justify-start lg:items-start lg:gap-2.5 lg:flex relative items-center justify-center text-center lg:text-start">
+          <div className=" flex-col lg:justify-start lg:items-start lg:gap-[15px] lg:inline-flex justify-center items-center text-center flex space-y-4 lg:space-y-0">
+            <div className=" h-[85px] flex-col lg:justify-start lg:items-start gap-[5px] flex text-center lg:text-start ">
+              <div className=" text-[#f6f7f8] text-lg font-semibold leading-[30px] flex text-center items-center justify-center lg:text-start ">
                 ¿Eres mayor de 18 años?
               </div>
-              <div className="self-stretch text-white text-lg font-normal leading-[25px]">
+              <div className="text-white text-lg font-normal leading-[25px] flex text-center items-center justify-center lg:text-start ">
                 Debes ser mayor de 18 años para ver este contenido.
               </div>
             </div>
-            <div className="justify-start items-start gap-[5px] inline-flex">
+            <div className="lg:justify-start lg:items-start gap-[5px] lg:inline-flex space-x-4 lg:space-x-0 ">
               <button
                 onClick={() => handleAgeVerification(false)}
                 className="px-[15px] py-[5px] bg-white rounded-[100px] flex-col justify-start items-center gap-[12.50px] inline-flex"
               >
-                <div className="justify-start items-center gap-[12.50px] inline-flex">
+                <div className="lg:justify-start items-center gap-[12.50px] inline-flex">
                   <div className="text-center text-[#362151] text-[15px] font-medium leading-[30px]">
                     Salir
                   </div>
@@ -108,7 +117,7 @@ const AgeVerification = () => {
                 onClick={() => handleAgeVerification(true)}
                 className="px-[15px] py-[5px] bg-gradient-to-b from-[#e05bff] to-[#863799] rounded-[100px] flex-col justify-start items-center gap-[12.50px] inline-flex"
               >
-                <div className="justify-start items-center gap-[12.50px] inline-flex">
+                <div className="lg:justify-start items-center gap-[12.50px] inline-flex">
                   <div className="text-center text-white text-[15px] font-medium leading-[30px]">
                     Soy mayor de 18
                   </div>
@@ -117,7 +126,9 @@ const AgeVerification = () => {
             </div>
           </div>
           <div className="w-[30px] h-[30px] relative" />
-          <HiOutlineX size={24} className="absolute ml-64" />
+          <button onClick={() => handleAgeVerification(false)} className="absolute ml-60 lg:block hidden">
+            <HiOutlineX size={24} />
+          </button>
         </div>
       </div>
     </div>
