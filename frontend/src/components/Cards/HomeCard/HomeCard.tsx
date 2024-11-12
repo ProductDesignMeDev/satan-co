03b893e49af24ab06231ccf56488fcdfc4b96968
AdrianEmaST <desarrolloadrianjr@gmail.com>
2024-11-12 19:30:26 -0300

    Add: PageHome/Desktop/Mobile

--- frontend/src/components/Cards/HomeCard/HomeCard.tsx
+++ frontend/src/components/Cards/HomeCard/HomeCard.tsx
@@ -1,22 +1,30 @@
 import React from "react";
 import Image from "next/image";
-import { Plantita } from "@/public";
 
-export default function HomeCard() {
+interface HomeCardProps {
+  imageSrc: string;
+  title: string;
+  isZoomed?: boolean;
+}
+
+export default function HomeCard({ imageSrc, title, isZoomed }: HomeCardProps) {
   return (
-    <main className="flex flex-col items-center lg:w-[375px] lg:h-[370px]">
-      <div>
+    <main className="flex flex-col items-center lg:w-[375px] lg:h-[370px] lg:mb-28 lg:mt-14">
+      <div
+        className={`flex justify-center lg:w-[375px] ${
+          isZoomed ? "lg:h-[390px] lg:mt-[-45px] mt-0" : "lg:h-[310px]"
+        } w-[328px] h-[213px]`}
+      >
         <Image
-          src={Plantita}
-          alt="Banner"
-          width={375}
-          height={310}
-          className="rounded-[20px] border-2 border-[#e05bff]"
+          src={imageSrc}
+          alt={title}
+          width={328}
+          height={213}
+          className="rounded-[20px] border-2 border-[#e05bff] object-cover lg:w-[375px] lg:h-full"
         />
       </div>
-      <div className="text-[#e05bff] text-2xl font-medium  leading-9 text-center mt-5">
-        {" "}
-        Spicy Banana
+      <div className="text-[#e05bff] text-2xl font-medium leading-9 text-center mt-5 mb-8">
+        {title}
       </div>
     </main>
   );
--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -6,7 +6,7 @@ export { default as Footer } from "./Layouts/Footer/Footer";
 //Exportaciones Card
 export { default as HomeCard } from "./Cards/HomeCard/HomeCard";
 export { default as TestimonyCard } from "./Cards/TestimonyCard/TestimonyCard";
+export { default as TestimonyCardCarousel } from "./Cards/TestimonyCard/TestimonyCardCarousel";
 
 //Exportaciones Accordion
 export { default as Accordeon } from "./Accordeon/Accordeon";
-
