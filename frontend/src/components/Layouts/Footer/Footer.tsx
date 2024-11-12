11bbe14ce55a63d0532d28df0941bba72205bb89
AdrianEmaST <desarrolloadrianjr@gmail.com>
2024-11-11 23:11:25 -0300

    Add: Footer Desktop/Mobile. Fix: 404Error vista Mobile Footer

--- frontend/src/components/Layouts/Footer/Footer.tsx
+++ frontend/src/components/Layouts/Footer/Footer.tsx
@@ -1,13 +1,79 @@
 import React from "react";
+import Image from "next/image";
+
+import {
+  HumoFooterDerecha,
+  HumoFooterIzquierda,
+  FuegoFooterSvg,
+  Logo,
+  IconInstagram,
+} from "@/public";
 
 export default function Footer() {
   return (
     <footer
       role="contentinfo"
-      className="bg-background text-white w-full relative  h-[340px]"
+      className="bg-background text-white w-full relative lg:h-[478px] h-[294px]"
     >
-      {/* Contenido del footer */}
-     Footer
+      
+      {/* Humos */}
+      <div className="flex flex-row justify-between items-end h-full">
+        <div className="flex bottom-0">
+          <Image
+            src={HumoFooterIzquierda}
+            alt="Logo"
+            width={130}
+            height={93}
+            className="lg:w-[398px] lg:h-[301px]"
+          />
+        </div>
+        <div>
+          <Image
+            src={HumoFooterDerecha}
+            alt="Logo"
+            width={130}
+            height={93}
+            className="lg:w-[398px] lg:h-[301px]"
+          />
+        </div>
+      </div>
+      <div className="absolute bottom-0">
+      <div className="flex flex-col items-center text-center justify-center space-y-3 mb-[40px] lg:mb-0">
+        <div>
+          <Image
+            src={Logo}
+            alt="Logo"
+            width={110}
+            height={98}
+            className="lg:w-[236px] lg:h-[209px]"
+          />
+        </div>
+        <div>
+          <p className="text-white font-medium lg:text-lg lg:leading-[27px] text-[15px] leading-snug">
+            Redes Sociales
+          </p>
+        </div>
+        <div className="flex flex-row lg:space-x-2">
+          <Image src={IconInstagram} alt="Logo" width={20} height={20} />
+          <p className="text-white lg:text-[15px] font-normal lg:leading-snug text-xs leading-[18px]">
+            satanyco.genetics
+          </p>
+        </div>
+        <div>
+          <p className="text-white lg:text-sm font-normal lg:leading-[21px] text-[10px] leading-[15px]">
+            © 2024 Satan&Co X Product Design Me
+          </p>
+        </div>
+      </div>
+        <Image
+          src={FuegoFooterSvg}
+          alt="Logo"
+          width={360}
+          height={41}
+          className="lg:w-full lg:h-[138px]"
+        />
+      </div>
+      
     </footer>
   );
 }
--- frontend/src/public/index.ts
+++ frontend/src/public/index.ts
@@ -1,17 +1,20 @@
 export { default as Logo } from "./Logo.svg";
 export { default as Banner } from "./Banner.jpeg";
-export { default as FuegoFooter } from "./FuegoFooter.png";
-export { default as NubeFooterDerecha } from "./NubeFooterDerecha.png";
-export { default as NubeFooterIzquierda } from "./NubeFooterIzquierda.png";
 export { default as NubesMobileMenu } from "./NubesMobileMenu.png";
-export { default as IconInstagram } from "./IconInstagram.png";
 export { default as Plantita } from "./Plantita.jpg";
 export { default as Error404 } from "./Error404.svg";
 export { default as FlechaIzquierda } from "./FlechaIzquierda.svg";
 export { default as IconCannabis } from "./IconCannabis.svg";
-export { default as HumoFooterIzquierda } from "./HumoFooterIzquierda.svg";
-export { default as HumoFooterDerecha } from "./HumoFooterDerecha.svg";
+
 export { default as HumoEdadIzquierda } from "./HumoEdadIzquierda.svg";
 export { default as HumoEdadDerecha } from "./HumoEdadDerecha.svg";
 export { default as Ellipse } from "./Ellipse.svg";
 export { default as EllipseDerecha } from "./EllipseDerecha.svg";
+
+//Footer
+export { default as HumoFooterIzquierda } from "./HumoFooterIzquierda.svg";
+export { default as HumoFooterDerecha } from "./HumoFooterDerecha.svg";
+export { default as FuegoFooterSvg } from "./FuegoFooter1.svg";
+
+//Iconos
+export { default as IconInstagram } from "./IconInstagram.svg";
