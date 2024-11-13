c7e9614492d1a565491da7fbfaeb8814932f0a09
LeandroBrangi <leandro.brangi@gmail.com>
2024-11-13 19:58:22 -0300

    Componentes Catalogo de semillas

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -6,7 +6,10 @@ export { default as Footer } from "./Layouts/Footer/Footer";
 //Exportaciones Card
 export { default as HomeCard } from "./Cards/HomeCard/HomeCard";
 export { default as TestimonyCard } from "./Cards/TestimonyCard/TestimonyCard";
+export { default as ProductCard } from "./Cards/ProductCard/ProductCard";
 
 //Exportaciones Accordion
 export { default as Accordeon } from "./Accordeon/Accordeon";
+
+
 
