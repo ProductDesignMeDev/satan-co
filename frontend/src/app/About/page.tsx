defd87fe502d7a813c3b38ee28b1432e64f79bb1
Facundo <facurael@gmail.com>
2024-11-18 23:34:59 -0300

    refactor: restructured imports, changed to default export, and improved mobile responsiveness in AwardsCards; updated styles in related components

--- frontend/src/app/About/page.tsx
+++ frontend/src/app/About/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { Metadata } from "next";
 import { HumoQuienes1, HumoQuienes2 } from "@/public";
-import { AboutSection } from "@/components/Cards/AboutCard/AboutSection";
+import { AboutSection } from "@/components";
 
 export const metadata: Metadata = {
   title: "Conoce Nuestro Equipo | Expertos en el Mundo del Cannabis",
@@ -18,14 +18,14 @@ export default function About() {
                 <Image
                   src={HumoQuienes1}
                   alt="Fuego izquierda"
-                  className="lg:w-[272px] lg:h-[479px] w-[120px] h-[230px] absolute lg:mt-28 mt-9  left-0 -z-50"
+                  className="lg:w-[272px] lg:h-[479px] w-[120px] h-[230px] absolute lg:mt-28 mt-96 sm:mt-12 left-0 -z-50"
                 />
             </div>
             <div>
                 <Image
                   src={HumoQuienes2}
                   alt="Fuego derecha"
-                  className="lg:w-[257px] lg:h-[479px] w-[120px] h-[230px] absolute lg:mt-28 mt-9 right-0 -z-50"
+                  className="lg:w-[257px] lg:h-[479px] w-[120px] h-[230px] absolute lg:mt-28 mt-96 sm:mt-12 right-0 -z-50"
                 />
             </div>
           </div>
@@ -35,8 +35,8 @@ export default function About() {
             ¿Quién es Satan&Co?
           </h2>
         </div>
-        <div className="w-full max-w-[328px] sm:max-w-[888px] bg-[#e05bff]/15 rounded-3xl flex items-center justify-center p-4 sm:p-0">
-          <div className="w-full max-w-[281px] sm:max-w-[812px] rounded-lg p-4 sm:p-8 flex items-center">
+        <div className="w-full max-w-[328px] sm:max-w-[888px] bg-[#e05bff]/15 rounded-3xl flex items-center justify-center p-4 sm:p-0 relative">
+          <div className="w-full max-w-[281px] sm:max-w-[812px] rounded-lg p-4 sm:p-8 flex items-center sm:ml-8">
             <div className="text-white font-poppins text-[14px] sm:text-[20px] leading-relaxed sm:leading-[1.6]">
               <p className="mb-2 sm:mb-4">
                 Somos un equipo de familia y amigos cuya pasión por el Cannabis nos
