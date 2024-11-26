fb78e0f2843b9a2c0426f60eb305ab5ddb87c731
LeandroBrangi <leandro.brangi@gmail.com>
2024-11-26 18:20:48 -0300

    fix next configuration

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'res.cloudinary.com', // Reemplaza con tu dominio
-          port: '', // Deja vacío si no hay un puerto específico
-          pathname: '/**', // Permite todas las rutas dentro de este dominio
-        },
-      ],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com', // Reemplaza con tu dominio
+                port: '',
+                pathname: '/**',
+            },
+        ],
     },
-  };
-  
-  module.exports = nextConfig;
+};
+
+export default nextConfig;
