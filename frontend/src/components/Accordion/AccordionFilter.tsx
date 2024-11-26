3ad215da4bdb4216a69c3925a98299e51e123c37
LeandroBrangi <leandro.brangi@gmail.com>
2024-11-26 01:56:16 -0300

    Fix visuales catalogo

--- frontend/src/components/Accordion/AccordionFilter.tsx
+++ frontend/src/components/Accordion/AccordionFilter.tsx
@@ -16,6 +16,10 @@ export default function AccordionFilter({
   const [accordionOpen, setAccordionOpen] = useState(false);
 
   const handleFilter = (property: string) => {
+    if (!Array.isArray(state)) {
+      console.error("Error: el estado no es un array:", state);
+      return;
+  }
     setAccordionOpen(!accordionOpen);
     if (state.includes(property)) {
       setState(state.filter((item: string) => item !== property));
@@ -28,32 +32,32 @@ export default function AccordionFilter({
     <div className="my-4 align-center justify-center text-white w-full">
       <button
         onClick={toggleAccordion}
-        className="flex justify-between items-center w-full"
+        className="flex justify-between items-center w-full lg:px-2 px-5"
       >
-        <div className="flex items-center justify-center space-x-4">
-          <h3 className="font-poppins lg:text-2xl text-lg text-white font-medium py-5 ml-1">
+        <div className="flex items-center justify-center space-x-4 ">
+          <h3 className="font-poppins lg:text-2xl text-lg text-white font-medium pt-5 pb-4 ml-1">
             {title}
           </h3>
         </div>
 
         <svg
-          className="fill-white shrink-0 ml-8 lg:w-4 lg:h-4 transition-transform duration-300 ease-out"
-          width="15"
-          height="15"
+          className="fill-white shrink-0 ml-8 lg:w-7 lg:h-7 w-5 h-5 transition-transform duration-300 ease-out"
+          width="24"
+          height="24"
           xmlns="http://www.w3.org/2000/svg"
         >
           {/* Línea horizontal (siempre visible) */}
           <rect
-            y="7"
-            width="15"
+            y="10"
+            width="24"
             height="2"
             rx="1"
             className="origin-center"
           />
           {/* Línea vertical (visible solo cuando está contraído) */}
           <rect
-            x="7"
-            height="15"
+            x="10"
+            height="24"
             width="2"
             rx="1"
             className={`origin-center transition-transform duration-300 ease-out ${isOpen ? "scale-0" : "scale-100"
@@ -63,7 +67,7 @@ export default function AccordionFilter({
       </button>
 
       <div
-        className={`grid overflow-hidden transition-all duration-300 ease-in-out text-white text-sm mt-1 ${isOpen ? "grid-rows-[1fr] opacity-100" : "grid-rows-[0fr] opacity-0"
+        className={`grid w-full overflow-hidden transition-all duration-300 ease-in-out text-white text-sm mt-1 lg:px-2 px-5 ${isOpen ? "grid-rows-[1fr] opacity-100" : "grid-rows-[0fr] opacity-0"
           }`}
       >
         <ol className="overflow-hidden">
@@ -71,7 +75,7 @@ export default function AccordionFilter({
             properties.map((property, index) => (
               <li
                 key={index}
-                className="flex justify-between py-2 ml-1 font-poppins text-sm"
+                className="flex justify-between lg:py-4 py-2 ml-1 font-poppins lg:text-xl font-normal text-base"
               >
                 <label htmlFor={property}>{property}</label>
                 <input
@@ -84,7 +88,7 @@ export default function AccordionFilter({
                   className="hidden" // Oculta el checkbox real
                 />
                 <div
-                  className={`w-6 h-6 border-2 rounded-sm flex items-center justify-center cursor-pointer
+                  className={`w-5 h-5 border-2 rounded-md flex items-center justify-center cursor-pointer
       ${state.includes(property) ? ' text-textColor3-500' : 'bg-transparent border-white'} 
       relative`}
                   onClick={() => handleFilter(property)}
--- frontend/src/components/FilterBar/FilterBar.tsx
+++ frontend/src/components/FilterBar/FilterBar.tsx
@@ -2,22 +2,31 @@
 
 import AccordionFilter from "../Accordion/AccordionFilter";
 import { SEED, SCENT, PLACE } from "@/types/product.model";
-import { CestoBasura } from "@/public";
+import { CestoBasura, CestoBasura2 } from "@/public";
 import Image from "next/image";
 import { catalogState } from "@/types";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function FilterBar({ state, setState, filter, resetFilter, stateFilter }: catalogState) {
     const [openAccordion, setOpenAccordion] = useState<string | null>(null); // Estado para controlar qué acordeón está abierto
+    const [isDesktop, setIsDesktop] = useState<boolean>()
 
     const toggleAccordion = (title: string) => {
         setOpenAccordion(openAccordion === title ? null : title);
     };
 
+    useEffect(() => {
+        if (window.innerWidth > 600) {
+            setIsDesktop(true)
+        } else {
+            setIsDesktop(false)
+        }
+    }, [])
+
 
     return (
         <section>
-            <h2 className="font-poppins lg:text-2xl text-xl text-textColor3 border-b-[3px] border-borderpink pb-4 pl-1">
+            <h2 className="font-poppins lg:text-3xl lg:px-4 text-xl text-textColor3 border-b-[3px] border-borderpink pb-4 px-5">
                 Filtrar por
             </h2>
             <AccordionFilter title="Tipo de semilla" properties={Object.values(SEED)} state={state} setState={setState} isOpen={openAccordion === "Tipo de semilla"} toggleAccordion={() => toggleAccordion("Tipo de semilla")} />
@@ -25,30 +34,38 @@ export default function FilterBar({ state, setState, filter, resetFilter, stateF
             <AccordionFilter title="Lugar de cultivo" properties={Object.values(PLACE)} state={state} setState={setState} isOpen={openAccordion === "Lugar de cultivo"} toggleAccordion={() => toggleAccordion("Lugar de cultivo")}
             />
 
-            <div className="flex md:flex-col sm:flex-row items-center justify-center gap-4 mt-6 md:w-[276px]">
+            <div className="flex md:flex-col sm:flex-row items-center justify-center gap-4 mt-5 md:w-[276px]">
                 <button
                     onClick={filter}
-                    className="font-poppins lg:text-prymary1 lg:text-2xl text-base bg-textColor3  rounded-3xl mb-5 md:w-[276px] md:h-[55px] w-[147px] h-[40px]  "
+                    className="font-poppins md:order-1 order-2 lg:text-prymary1 lg:text-2xl font-normal text-base rounded-full bg-textColor3 lg:mb-5 mb-5 md:w-[276px] md:h-[55px] w-[147px] h-[40px]  "
                 >
                     Aplicar filtro
                 </button>
-                
-                    <button
-                        onClick={resetFilter}
-                        className={`flex flex-row justify-center lg:p-2 items-center font-poppins md:text-xl font-normal text-base  border-2 border-textColor4 rounded-3xl mb-5  md:w-[276px] md:h-[55px] w-[147px] h-[40px]
-                        ${stateFilter.length > 0 ? 'text-prymary1 bg-textColor3 border-white' : 'text-textColor4 bg-transparent border-textColor4'}`}
-                    >
-                        Borrar filtro
-                        <Image
+
+                <button
+                    onClick={resetFilter}
+                    className={`flex flex-row md:order-2 order-1 justify-center lg:p-2 items-center font-poppins lg:text-2xl font-normal text-base rounded-full border-2 mb-5 lg:w-[276px] lg:h-[55px] w-[147px] h-[40px]
+                        ${stateFilter.length > 0 || state.length > 0 ? 'text-textColor3  border-textColor3' : 'text-textColor4 border-textColor4'}`}
+                >
+                    Borrar filtro
+                    {isDesktop && (
+                        stateFilter.length > 0 || state.length > 0 ? <Image
                             className="ml-5"
                             src={CestoBasura}
                             alt="Limpiar filtros"
-                            width={20}
-                            height={20}
+                            width={24}
+                            height={24}
+                        /> : <Image
+                            className="ml-5"
+                            src={CestoBasura2}
+                            alt="Limpiar filtros"
+                            width={24}
+                            height={24}
                         />
-                    </button>
-                </div>
-            
+                    )}
+                </button>
+            </div>
+
         </section>
     );
 }
