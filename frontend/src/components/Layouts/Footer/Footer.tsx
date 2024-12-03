ea62e659d8d266f9ffbcec8be71daab07f13d88e
AdrianEmaST <desarrolloadrianjr@gmail.com>
2024-12-03 15:00:21 -0300

    Fix: Diseño

--- frontend/src/components/Layouts/Footer/Footer.tsx
+++ frontend/src/components/Layouts/Footer/Footer.tsx
@@ -10,6 +10,7 @@ import {
   HumoFooterIzquierdaPNG,
   Fuegofooter300Pgn,
 } from "@/public";
+import Link from "next/link";
 
 export default function Footer() {
   const pathname = usePathname(); // Obtiene la ruta actual
@@ -40,31 +41,37 @@ export default function Footer() {
         </div>
       </div>
       <div className="absolute bottom-0 items-center text-center justify-center">
-        <div className="flex flex-col items-center text-center justify-center space-y-3 mb-[40px] lg:mb-0">
+        <div className="flex flex-col items-center text-center justify-center mb-[40px] lg:mb-0">
           <div>
             <Image
               src={Logo}
               alt="Logo"
-              className="lg:w-[236px] lg:h-[209px] w-[110px] h-[98px]"
+              className="lg:w-[236px] lg:h-[209px] w-[110px] h-[98px] lg:mb-[19px] mb-0"
             />
           </div>
           <div>
-            <p className="text-white font-medium lg:text-lg lg:leading-[27px] text-[15px] leading-snug">
+            <p className="text-white font-medium lg:text-lg lg:leading-[27px] text-[15px] leading-snug lg:mb-[5px] mb-0">
               Redes Sociales
             </p>
           </div>
-          <div className="flex flex-row lg:space-x-1">
-            <Image
-              src={IconInstagram}
-              alt="Logo"
-              className="w-[25px] h-[25px]"
-            />
-            <p className="text-white lg:text-[15px] font-normal lg:leading-snug text-xs leading-[18px]">
-              satanyco.genetics
-            </p>
-          </div>
+          <Link
+            href="https://www.instagram.com/satanyco.genetics/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="flex flex-row lg:space-x-1 lg:mb-[30px] mb-[15px] items-center">
+              <Image
+                src={IconInstagram}
+                alt="Logo"
+                className="w-[25px] h-[25px]"
+              />
+              <p className="text-white lg:text-[15px] font-normal lg:leading-snug text-xs leading-[18px]">
+                satanyco.genetics
+              </p>
+            </div>
+          </Link>
           <div>
-            <p className="text-white lg:text-sm font-normal lg:leading-[21px] text-[10px] leading-[15px]">
+            <p className="text-white lg:text-sm font-normal lg:leading-[21px] text-[10px] leading-[15px] lg:mb-[17px] mb-0">
               © 2024 Satan&Co X Product Design Me
             </p>
           </div>
