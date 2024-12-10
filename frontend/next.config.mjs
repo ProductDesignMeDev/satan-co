031863487a57de6b1866c863d785ab80de930556
AdrianEmaST <desarrolloadrianjr@gmail.com>
2024-12-09 23:41:40 -0300

    Fix: AgeVerification Add: Hotjar

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,15 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'res.cloudinary.com', // Reemplaza con tu dominio
-                port: '',
-                pathname: '/**',
-            },
-        ],
-    },
+  
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com", // Reemplaza con tu dominio
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
+
 
 export default nextConfig;
