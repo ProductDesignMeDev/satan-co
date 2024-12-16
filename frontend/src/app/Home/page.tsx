d362639bd52855ae7b4804cf7000eda2e7ee26a2
AdrianEmaST <desarrolloadrianjr@gmail.com>
2024-12-16 12:06:22 -0300

    Fix: CardHome

--- frontend/src/app/Home/page.tsx
+++ frontend/src/app/Home/page.tsx
@@ -26,16 +26,19 @@ export default function HomePage() {
       imageSrc:
         "https://res.cloudinary.com/dp59p3lfo/image/upload/v1731532559/satanico-Semilla-foto-fem-Bud%C3%ADn-2.jpg",
       title: "Spicy Banana",
+      link: "/Catalog/Detail/5?title=Spicy%20Banana&description=En%20esta%20cruza%20de%20un%20clon%20de%20Strawberry%20Banana%20seleccionado%20por%20un%20amigo%20cultivador%20uruguayo%20y%20la%20Gelatto%2033%2C%20podemos%20encontrar%20un%20inigualable%20olor%20a%20Banana%20Split%2C%20con%20toques%20picantes%20y%20cremosos%2C%20consiguiendo%20tanto%20en%20interior%20y%20exterior%20excelente%20rendimiento.%20Tiene%20excelente%20devolución%20en%20las%20diferentes%20técnicas%20de%20extracción%20sin%20solvente.&seed=Foto%252FFEM%2C%20High%2520THC",
     },
     {
       imageSrc:
         "https://res.cloudinary.com/dp59p3lfo/image/upload/v1733776592/Semilla-foto-fem-Bud%C3%ADn-1_e5qfva.jpg",
       title: "Budín",
+      link: "/Catalog/Detail/9?title=Budín&description=Este%20cruce%20de%20nuestros%20dos%20cortes%20preferidos%20de%20Wedding%20Cake%2C%20nos%20entrega%20un%20exquisito%20aroma%20a%20Budín%20de%20limón%20y%20frutos%20secos%20con%20toques%20de%20frutos%20rojos%20y%20caramelo%20que%20perduran%20en%20nuestro%20paladar%20aún%20al%20tiempo%20de%20apagarla.%20Poseedora%20de%20una%20buena%20distancia%20internodal%2C%20lo%20que%20la%20hace%20nuestra%20compañera%20ideal%20para%20el%20cultivo%20exterior.&seed=Foto%252FFEM%2C%20High%2520THC",
     },
     {
       imageSrc:
         "https://res.cloudinary.com/dp59p3lfo/image/upload/v1733777873/SunsetFire_ivlhtg.jpg",
       title: "Sunset Fire",
+      link: "/Catalog/Detail/6?title=Sunset%20Fire&description=Ananá%2C%20crema%2C%20manzana%2C%20caramelos%20y%20chicles%2C%20son%20sólo%20algunas%20de%20las%20palabras%20que%20se%20nos%20vienen%20a%20la%20mente%20cuando%20olemos%20y%20saboreamos%20una%20Sunset%20Fire.%20Este%20multipremiado%20cruce%20de%20Gelato%2033%20y%20Sunset%20Sherbet%20viene%20a%20sorprendernos%20con%20su%20impresionante%20vigor%2C%20excelente%20producción%20y%20mezcla%20de%20olores%20extremadamente%20complejo&seed=Foto%252FFEM%2C%20High%2520THC",
     },
   ];
 
@@ -105,6 +108,7 @@ export default function HomePage() {
               imageSrc={card.imageSrc}
               title={card.title}
               isZoomed={index === 1}
+              link={card.link}
             />
           ))}
         </div>
